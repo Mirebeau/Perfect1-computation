cbdc00b997c053548497628a43f7c500ee9f6899
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2023-06-05 18:50:33 +0200

    Cleanup and add results

--- Perfect1.py
+++ Perfect1.py
@@ -61,115 +61,6 @@ def Voronoi(D,maxiter=100):
 	assert np.allclose(val,np.sum(Dold*Mopt))
 	return Mopt,(i,g)
 
-def cross(A):
-	"""
-	Compute the generalized cross product. 
-	A is a matrix of shape (n,n-1,...), representing a family of n-1 vectors.
-	"""
-	return np.array([(-1)**i*
-		np.linalg.det(np.moveaxis(np.delete(A,i,axis=0),(0,1),(-2,-1))) for i in range(len(A))])
-
-def to_hashable(M): return tuple(map(tuple,M)) # Sets can only contain hashable elements
-
-
-
-
-def Perfect1(ndim,iref,retest=False,verbosity=1,
-	linprog_kwargs=None,in_perfect1_tests=None):
-	"""
-	Compute the set Perfect1(M0), the where M0 is a reference perfect form. It is defined in
-	Bonnans, Bonnet, Mirebeau, Monotone discretization of anisotropic differential operators 
-	using Voronoi’s first reduction.
-	Computing this set allows to bound the size of the vectors appearing in Voronoi's decomposition.
-	"""
-	if linprog_kwargs is None: linprog_kwargs = dict()
-	Ryskov_data = Ryskov_load(ndim)
-	# Construct the symmetric matrices DE associated to all subsets of active constraints.
-	Ξ = Ryskov_data[iref].Ξ
-	E = np.array(list(itertools.combinations(range(Ξ.shape[1]),ndim-1))).T
-	E = cross(Ξ[:,E])
-	E = E[:,np.any(E,axis=0)]
-	D0 = outer_self(E).sum(axis=-1)
-	
-	Mlcm = np.lcm.reduce([x.M[0,0] for x in Ryskov_data]) # Normalize the minimal value
-	Ms = np.stack([x.M*Mlcm/x.M[0,0] for x in Ryskov_data],axis=-1).round().astype(int) 
-	GT = np.moveaxis(Ryskov_data[iref].G,0,1) # Perfect1 is invariant under Transposed isometries
-
-	# Testing wether a perfect form belongs to Perfect1(M0) is costly. 
-	# We thus tag previously visited forms, as well as their equivalence class under isometries.
-	visited = set()
-	def is_new(M):
-		"""Check wether M, or some other isometrically equivalent form, has already been visited"""
-		if to_hashable(M) in visited: return False
-		Meq = np.unique(dot_AtDA(M,GT),axis=-1) # Equivalence class of M modulo the isometry group.
-		visited.update([to_hashable(x) for x in np.moveaxis(Meq,-1,0)])
-		return True
-
-	# Tests wether a perfect form belongs to Perfect1 by solving a linear program
-	#lps = [] # Save the data associated to previously solved linear programs, just in case
-	def in_perfect1(i,g,verb=verbosity):
-		M = dot_AtDA(Ms[...,i],g)
-		data = Ryskov_data[i]
-		neigh_M = dot_AtDA(Ms[...,data.neigh_i],dot(data.neigh_g,g))
-		# Constraint Tr(D(μ) M) <= Tr(D(μ) M') for all neighbors M' of M is implemented as Ax<=0
-		A = norm2_AV(M,E) - norm2_AV(neigh_M[:,:,:,np.newaxis],E[:,np.newaxis,:])
-		b = np.zeros(neigh_M.shape[-1])
-		c = np.ones(E.shape[-1]) # Arbitrary objective function
-		# We look for a solution x with positive coefficients x>0. By homogeneity, we can look for x>=1
-		res = linprog(c,A,b,bounds=(1,None),**linprog_kwargs) 
-		# Status : 0 -> feasible, 2 -> unfeasible. 
-		# https://docs.scipy.org/doc/scipy/reference/optimize.linprog-highs.html#optimize-linprog-highs
-		if res['status'] not in [0,2]: print(f"Issue with linear program : {res['status']=}",
-			f"Inputs : {i=}, {g=}. Output : {res}")
-		#lps.append([(i,g),(A,b,c),res])
-		print(f"Linear program found status {res['status']} for \n{M}")
-		if verb: print(f"and result {res}")
-		return res['status']==0
-
-	if in_perfect1_tests is not None:
-		if isinstance(in_perfect1_tests,tuple) and len(in_perfect1_tests)==2:
-			in_perfect1_tests = [in_perfect1_tests]
-		print("Testing data : ",in_perfect1_tests)
-		print([in_perfect1(i,g) for (i,g) in in_perfect1_tests])
-		return None,None,None
-
-	# Compute Perfect1 iteratively, keeping only a representative of each class.
-	_,(i0,g0) = Voronoi(D0); g0 = g0.astype(int)
-	assert in_perfect1(i0,g0) 
-	is_new(dot_AtDA(Ms[...,i0],g0))
-	perfect1 = []
-	new = [(i0,g0)]
-
-	while len(new)>0:
-		old=new; new = []
-		for (i0,g0) in old: 
-			# Look at the elements of perfect1 discovered in last step, enumerate their neighbors
-			perfect1.append((i0,g0))
-			data = Ryskov_data[i0]
-			neigh_g = dot(data.neigh_g,g0)
-			neigh_M = dot_AtDA(Ms[...,data.neigh_i],neigh_g)
-			# Visit all neighbors the neighbors and test if they belong to perfect1
-			for (i,g,M) in zip(data.neigh_i,np.moveaxis(neigh_g,-1,0),np.moveaxis(neigh_M,-1,0)):
-				if is_new(M) and in_perfect1(i,g): # Note : is_new has a side effect.
-					new.append((i,g))
-
-	# For convenience, return the full equivalence classes mod isometries of initial perfect form.
-	perfect1_full = [np.unique(dot_AtDA(Ms[...,i],dot(g,GT)),axis=-1) for (i,g) in perfect1]
-
-	if retest: # Optional, was mostly for debug purposes.
-		print("--------- Rechecking some of perfect1_full --------")
-		for (i,g) in perfect1:
-			assert in_perfect1(i,g,verb=0) 
-			for gt in np.moveaxis(GT,-1,0)[:20]:
-				assert in_perfect1(i,dot(g,gt),verb=0)
-
-	# Basic report
-	perfect1_full_n = [x.shape[-1] for x in perfect1_full]
-	print(f"Perfect1({Ryskov_data[iref].name}) has {sum(perfect1_full_n)} elements, split in "
-		f"equivalence classes of cardinality {perfect1_full_n} modulo isometries of {Ryskov_data[iref].name}")
-	return perfect1,perfect1_full
-
-
 def flatten_symmetric_matrix(m):
 	"""
 	Input : a square (symmetric) matrix.
@@ -179,67 +70,14 @@ def flatten_symmetric_matrix(m):
 	assert d==m.shape[1]
 	return np.concatenate([m[i,:(i+1)] for i in range(d)],axis=0)
 
-def _np_containssorted(arr,key): 
-	pos = np.searchsorted(arr,key)
-	if pos>=arr.size: return False
-	return arr[pos]==key
-
-class myset:
+def cross(A):
 	"""
-	This class is used to replace the python sets in a memory efficient way for the task at hand.
-	It reasonnably computationally efficient for it, given that insertions are relatively rare,
-	though definitely not optimal.
+	Compute the generalized cross product. 
+	A is a matrix of shape (n,n-1,...), representing a family of n-1 vectors.
 	"""
+	return np.array([(-1)**i*
+		np.linalg.det(np.moveaxis(np.delete(A,i,axis=0),(0,1),(-2,-1))) for i in range(len(A))])
 
-# This one is rubbish, eventually, for the task at hand.
-# Could probably do better by using a set + an array, instead of two arrays.
-
-	def __init__(self,elem_sample):
-		self.external_sample = elem_sample
-		self.external_dtype = elem_sample.dtype
-		self.external_size = elem_sample.size
-		uint_t = np.uint64 #np.int16
-		dtype_ratio = int(np.iinfo(uint_t).bits/np.iinfo(elem_sample.dtype).bits)
-		self.internal_size = int(np.ceil(self.external_size/dtype_ratio))
-		self.internal_dtype = np.dtype([(f"{i}",uint_t) for i in range(self.internal_size)])
-		self.padding = dtype_ratio*self.internal_size - self.external_size
-
-		self.empty = np.empty((0,),dtype=self.internal_dtype)
-		self.data = self.empty.copy()
-		self.temp = self.empty.copy()
-
-	def contains(self,key):
-		assert np.all(np.abs(key)<=np.iinfo(self.external_size).max)
-		key = np.pad(np.reshape(key.astype(self.external_dtype),(self.external_size,)),
-			(0,self.padding)).view(self.internal_dtype)
-		return _np_containssorted(self.data,key) or _np_containssorted(self.temp,key)
-
-	def insert(self,keys):
-		"""Insert keys, which are not already present."""
-		assert np.all(np.abs(keys)<=np.iinfo(self.external_size).max)
-		keys = np.moveaxis(np.pad(np.reshape(keys.astype(self.external_dtype),(self.external_size,-1)),
-			((0,self.padding),(0,0))),0,-1).reshape(-1).view(self.internal_dtype)
-
-		# Insert in the temporary dataset (which is reasonnably small)
-		keys.sort()
-		ii = np.searchsorted(self.temp,keys)
-		self.temp = np.insert(self.temp,ii,keys)
-#		temp_copy = self.temp.copy()
-#		self.temp.sort()
-#		assert np.all(self.temp == temp_copy)
-
-		# Merge into the large dataset if temp has become too large
-		if self.temp.size <= np.sqrt(self.data.size): return
-		ii = np.searchsorted(self.data,self.temp)
-#		assert np.all(self.data[:-1]<=self.data[1:]) # before insert
-		self.data = np.insert(self.data,ii,self.temp)
-#		data_copy = self.data.copy()
-#		self.data.sort()
-#		assert np.all(self.data==data_copy)
-#		if not np.all(data_copy==self.data):
-#			print(self.temp,data_copy
-##		assert np.all(self.data[:-1]<=self.data[1:]) # after insert
-		self.temp = self.empty.copy()
 
 def import_prev_run(filename):
 	import ast,re
@@ -260,11 +98,17 @@ def import_prev_run(filename):
 		assert status.size==elems.shape[-1]
 		return status,elems	
 
+def _np_containssorted(arr,key): 
+	"""Wether a numpy sorted array contains a given key"""
+	pos = np.searchsorted(arr,key)
+	if pos>=arr.size: return False
+	return arr[pos]==key
+
 class myset:
 	"""
 	This class is used to replace the python sets in a memory efficient way for the task at hand.
+	(Only really useful for the 5D cases)
 	"""
-
 	def __init__(self,init):
 		self.external_dtype = init.dtype
 		self.external_size = init.shape[0]
@@ -300,8 +144,7 @@ class myset:
 		self.temp.update(map(tuple,keys))
 
 		# Merge into the large dataset if temp has become too large
-		temp_min_size = 2e6; temp_ratio = 0.01
-#		temp_min_size = 10;	temp_ratio = 0.3
+		temp_min_size = 1e7; temp_ratio = 0.05
 		if len(self.temp) >= temp_min_size and len(self.temp)>=temp_ratio*len(self.data):
 			temp_data = np.sort(np.array(list(self.temp)).view(self.internal_dtype).reshape(-1))
 			self.temp = set()
@@ -309,7 +152,7 @@ class myset:
 			self.data = np.insert(self.data,ii,temp_data)
 
 
-def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
+def Perfect1(ndim,iref,retest=False,verbosity=1,init_filename=None,
 	linprog_kwargs=None,in_perfect1_tests=None,small_t=np.int16):
 	"""
 	Compute the set Perfect1(M0), the where M0 is a reference perfect form. It is defined in
@@ -330,21 +173,19 @@ def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
 	Ms = np.stack([x.M*Mlcm/x.M[0,0] for x in Ryskov_data],axis=-1).round().astype(int) 
 	GT = np.moveaxis(Ryskov_data[iref].G,0,1) # Perfect1 is invariant under Transposed isometries
 
-	# Testing wether a perfect form belongs to Perfect1(M0) is costly. 
-	# We thus tag previously visited forms, as well as their equivalence class under isometries.
-	big_t = np.int64
-	def flatten(M):
-		return np.pad(flatten_symmetric_matrix(M).astype(small_t),((0,self.padding),(0,0))).view(dtype)
-
+	# Optionally, we can tecover the list of all the perfect forms which were discarded 
+	# (do not belong to perfect1) in a previous run of the algorithm
 	if init_filename is None:
 		visited = myset(flatten_symmetric_matrix(D0).astype(small_t))
-	else: # Recover the list of all the previously tested elements outside perfect1
+	else: 
 		print(f"Importing output of previous run")
 		status,elems = import_prev_run(init_filename)
 		print(f"Previous run checked {status.size} equivalence classes"
 			"Recovering the {(status==2).size} which were discarded")
 		elems = elems[:,:,status==2].astype(small_t)
 #		new = list( np.moveaxis(elems,-1,0)[status==0] ) # We would need (i,g) rather than M here
+		# When applying the isometries to the previously computed elements, we need to make 
+		# We cannot deal with all of them at once, as this would use too much memory (D5 case)
 		elems_split = np.array_split(elems,elems.shape[-1]//1000 +1,axis=-1)
 		elems_eq = [] # full equivalence classes
 		for i,elemsi in enumerate(elems_split):
@@ -354,25 +195,14 @@ def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
 		elems_eq = np.moveaxis(np.concatenate(elems_eq,axis=0,dtype=small_t),0,-1)
 		visited = myset(elems_eq)
 
+	# Testing wether a perfect form belongs to Perfect1(M0) is costly. 
+	# We thus tag previously visited forms, as well as their equivalence class under isometries.
 	def is_new(M):
 		"""Check wether M, or some other isometrically equivalent form, has already been visited"""
 		if visited.contains(flatten_symmetric_matrix(M)): return False
 		Meq = dot_AtDA(M,GT) # Equivalence class of M modulo the isometry group.
 		visited.insert(np.unique(flatten_symmetric_matrix(Meq),axis=-1)) 
-#		print(f"--- INSERTION in visited ---\n{flatten_symmetric_matrix(M)}\n{visited.temp=},\n{visited.data=}") #,\n{M=},\n{Meq=}")
 		return True
-
-	visited_ = set()
-	def is_new_(M):
-		"""Check wether M, or some other isometrically equivalent form, has already been visited"""
-#		if to_hashable(M) in visited: return False
-		if tuple(flatten_symmetric_matrix(M)) in visited_: return False
-		# Equivalence class of M modulo the isometry group.
-		Meq = np.unique(flatten_symmetric_matrix(dot_AtDA(M,GT)),axis=-1) 
-		visited_.update([tuple(x) for x in np.moveaxis(Meq,-1,0)])
-#		print(f"--- INSERTION in visited_ (set) ---\n {flatten_symmetric_matrix(M)} {visited_=}")
-		return True
-
 
 	# Tests wether a perfect form belongs to Perfect1 by solving a linear program
 	lps = [] # Save the data associated to previously solved linear programs, just in case
@@ -395,6 +225,7 @@ def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
 		if verb: print(f"and result {res}")
 		return res['status']==0
 
+	# For debug purposes, we can check wether some given perfect forms belong to perfect1
 	if in_perfect1_tests is not None:
 		if isinstance(in_perfect1_tests,tuple) and len(in_perfect1_tests)==2:
 			in_perfect1_tests = [in_perfect1_tests]
@@ -405,7 +236,6 @@ def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
 	# Compute Perfect1 iteratively, keeping only a representative of each class.
 	_,(i0,g0) = Voronoi(D0); g0 = g0.astype(int)
 	assert in_perfect1(i0,g0) 
-	is_new_(dot_AtDA(Ms[...,i0],g0))
 	is_new(dot_AtDA(Ms[...,i0],g0))
 	new = [(i0,g0)]
 
@@ -420,13 +250,8 @@ def Perfect1_rev(ndim,iref,retest=False,verbosity=1,init_filename=None,
 			neigh_M = dot_AtDA(Ms[...,data.neigh_i],neigh_g)
 			# Visit all neighbors the neighbors and test if they belong to perfect1
 			for (i,g,M) in zip(data.neigh_i,np.moveaxis(neigh_g,-1,0),np.moveaxis(neigh_M,-1,0)):
-				is_new_val = is_new(M)
-#				if is_new_val!=is_new(M):
-#					print(f"Error : {is_new_val=},{M=},{len(visited_)=}")
-#					raise ValueError
-				if is_new_val and in_perfect1(i,g): # Note : is_new has a side effect.
+				if is_new(M) and in_perfect1(i,g): # Note : is_new has a side effect.
 					new.append((i,g))
-
 
 	# For convenience, return the full equivalence classes mod isometries of initial perfect form.
 	perfect1_full = [np.unique(dot_AtDA(Ms[...,i],dot(g,GT)),axis=-1) for (i,g) in perfect1]
@@ -464,7 +289,7 @@ if __name__ == "__main__":
 	in_perfect1_tests = None if len(sys.argv)<=4 else ast.literal_eval(sys.argv[4])
 	init_filename = None if len(sys.argv)<=5 else sys.argv[5]
 	
-	p1,p1f,lps = Perfect1_rev(d,i,verbosity=0,
+	p1,p1f,lps = Perfect1(d,i,verbosity=0,
 		linprog_kwargs=linprog_kwargs,in_perfect1_tests=in_perfect1_tests,init_filename=init_filename)
 	print(f"Representatives of Perfect1 modulo isometries : {p1}")
 #	print([np.moveaxis(x,-1,0) for x in p1f]) # Show all elements
